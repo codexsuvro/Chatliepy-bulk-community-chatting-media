e6b2fda3d17a30f2d81f5ff56e08ccb15f23c7a1
Suvrodeep Debnath <111605470+codexsuvro@users.noreply.github.com>
2023-07-18 03:21:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="a349e39a-0daa-49ad-853b-f76eb62e0000"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import axios from 'axios';
+
+const LoginForm = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const authObject = { 'Project-ID': "a349e39a-0daa-49ad-853b-f76eb62e0000", 'User-Name': username, 'User-Secret': password };
+    try {
+        await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+        window.location.reload();
+        // setError('');
+    } catch (error) {
+        setError('Oops, incorrect credentials.')
+    }
+  }
+    
+  return (
+    <div className="wrapper">
+        <div className="form">
+            <h1 className="title">Chat Application</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                <div align="center">
+                    <button type="submit" className="button">
+                        <span>Start Chatting</span>
+                    </button>
+                </div>
+                <h2 className="error" align="center">{error}</h2>
+            </form>
+        </div>
+    </div>
+  );
+};
+
+export default LoginForm;
